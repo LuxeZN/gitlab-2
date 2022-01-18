b2a519f63284517ad6f87cbcd3ea1132791fc55f
Zachary Naymik <zn475921@ohio.edu>
2022-01-18 11:40:50 -0500

    Hello Gitgit add git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Zachary Naymik
+ *        Date: 1/18/22
+ * Description: Program for git-lab2 repository to update the online main branch
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
